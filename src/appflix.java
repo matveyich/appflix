a470e38079cb2d94ca1891f85063089e838e2ab9
ilya matvienko <matveyich@gmail.com>
2015-12-03 00:42:33 +0200

    user applist

--- src/appflix.java
+++ src/appflix.java
@@ -4,31 +4,59 @@ public class appflix {
 	private static List<developer> devs = new ArrayList<developer>();
 	private static List<user> users = new ArrayList<user>();
 	private static List<application> storeApps = new ArrayList<application>();
+	private static int numberOfDevelopers = 2;
+	private static int numberOfUsers = 1;
 	
+	private static int maxAppsPerUser = 20;
+	private static int maxAppsPerDeveloper = 6;
+	private static float maxAppPrice = (float) 1000.0;
+	private static float minAppPrice = (float) 0.0;
+
+	private static float userMonthlyFee = (float) 20.0;
+	private static float platformFee = (float) 0.1;
 	
 	public static void main(String[] args) {
-		generateDevelopers(2);
-		generateUsers(1);
+		generateDevelopers(numberOfDevelopers);
+		generateUsers(numberOfUsers);
 		System.out.println("finished");
 	}
 	
 	public static void generateDevelopers(int numberOfDevelopers){
 		int i = 0;
 		developer dev;
-		
 		Random randomno = new Random();
+		
 		while (i < numberOfDevelopers){
-			dev = new developer("dev" + i, randomno.nextInt(6));
+			dev = new developer("dev" + i, randomno.nextInt(maxAppsPerDeveloper));
 			devs.add(dev);
 			storeApps.addAll(dev.getApps());
 			i++;
 		}
+		
+		int storeSize = storeApps.size();
+		if (storeSize < maxAppsPerUser) {
+			maxAppsPerUser = storeSize;
+		}
 	}
 	
 	public static void generateUsers(int numberOfUsers){
-		int i = 0;
+		int i = 0; 
+		int	k = 0;
+		user u;
+		Random randomno = new Random();
+		int numberOfApps = 0;
+		
 		while (i < numberOfUsers){
-			users.add(new user("user" + i));
+			u = new user("user" + i);
+			numberOfApps = randomno.nextInt(maxAppsPerUser);
+			k = 0;
+			
+			while (k < numberOfApps){
+				u.addApp(storeApps.get(randomno.nextInt(storeApps.size())));
+				k++;
+			}
+			
+			users.add(u);
 			i++;
 		}
 	}
--- src/user.java
+++ src/user.java
@@ -1,13 +1,22 @@
+import java.util.*;
 
 public class user {
 	private String name;
-	private application[] apps;
+	private List<application> apps = new ArrayList<application>();
 	
 	public user(String name){
 		this.name = name;
 	}
 	
+	public void addApp(application app){
+		this.apps.add(app);
+	}
+	
 	public String getInfo(){
 		return this.name;
+	}
+	
+	public List<application> getApps() {
+		return this.apps;
 	}
 }
