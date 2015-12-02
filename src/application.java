9fb0604fd660814f248ab7e6a762c60b107bd5e4
Ilya Matvienko <matveyich@gmail.com>
2015-12-02 17:43:17 +0200

    initial

---
+++ src/application.java
@@ -0,0 +1,11 @@
+
+public class application {
+	private String name;
+	private Float launchesPerDay;
+	private Float cost;
+	public application(String name, Float launchesPerDay, Float cost){
+		this.cost = cost;
+		this.launchesPerDay = launchesPerDay;
+		this.name = name;
+	}
+}
