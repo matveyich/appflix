216d7a2b1bca112b79565971cb5a028eb115535a
Ilya Matvienko <matveyich@gmail.com>
2015-12-02 19:38:19 +0200

    developers, apps, and users generation for the store

--- src/appflix.java
+++ src/appflix.java
@@ -1,11 +1,28 @@
+import java.util.*;
 
 public class appflix {
-
+	private static List<developer> devs = new ArrayList<developer>();
+	private static List<user> users = new ArrayList<user>();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		user u1 = new user("vasya");
-		developer d1 = new developer("dev1", 5);
-		System.out.println(d1.getInfo());
+		generateDevelopers(30);
+		generateUsers(10000);
+		System.out.println("finished");
 	}
-
+	public static void generateDevelopers(int numberOfDevelopers){
+		int i = 0;
+		Random randomno = new Random();
+		while (i < numberOfDevelopers){
+			devs.add(new developer("dev" + i, randomno.nextInt(6)));
+			i++;
+		}
+	}
+	public static void generateUsers(int numberOfUsers){
+		int i = 0;
+		while (i < numberOfUsers){
+			users.add(new user("user" + i));
+			i++;
+		}
+	}
+	
 }
